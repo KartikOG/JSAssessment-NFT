6301f86cb1982e042121049d7d2533866eca6084
Kartik Sharma <92888405+KartikOG@users.noreply.github.com>
2024-05-26 21:21:55 +0530

    js-assesment-nft.js

--- js-assesment-nft.js
+++ js-assesment-nft.js
@@ -1,21 +1,5 @@
-/*
-Assessment Requirements
-1. Create a variable that can hold a number of NFT's. What type of variable might this be?
-2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
-   The metadata values will be passed to the function as parameters. When the NFT is ready, 
-   you will store it in the variable you created in step 1
-3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
-4. For good measure, getTotalSupply() should return the number of NFT's you have created
-*/
-
-// create a variable to hold your NFT's
-
 let nfts = []; // An Array
-
-// this function will take in some values as parameters, create anṇ
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
-function mintNFTs (name,info,link) {
+function mintNFTs (name,info,link) { //to mint the multple nfts
     const nft = {
         name,
         info,
@@ -24,9 +8,7 @@ function mintNFTs (name,info,link) {
     nfts.push(nft);
 }
 
-// create a "loop" that will go through an "array" of NFT's
-// and print their metadata with console.log()
-function listNFTs () {
+function listNFTs () { //print the metadata of nfts
     for(let j=0; j<nfts.length;j++){
         console.log(`Name: ${nfts[j].name}`);
         console.log(`Info:,${nfts[j].info}`);
@@ -34,13 +16,10 @@ function listNFTs () {
     }
 }
 
-// print the total number of NFTs we have minted to the console
-function getTotalSupply() {
+function getTotalSupply() { // print the total number of NFTs we have minted to the console
     console.log("Number of NFT's minted are : ",nfts.length);
 }
-
-// call your functions below this line
-
+// Calling the funtions
 mintNFTs("Dodgecat","A digital cat currency","https://meow.com/DodgeCat");
 mintNFTs("BitDog", "A digital dog currency","https://bow.com/BitDog");
 mintNFTs("EtheFox","A digital fox currency","https://raspybark.com/EtheFox");
